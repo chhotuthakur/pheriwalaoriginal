a3cf070b43af3d5147c4ca27b7d422729783f810
chhotuthakur <npsdahiya@gmail.com>
2023-04-05 10:32:11 +0530

    updated ui and more...

--- app/src/main/java/com/omkar/pheriwala/activities/SplashActivity.java
+++ app/src/main/java/com/omkar/pheriwala/activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.omkar.pheriwala.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.omkar.pheriwala.R;
 
@@ -12,5 +14,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+new Handler().postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        startActivity(new Intent(SplashActivity.this,AccountActivity.class));
+    }
+},5000);
     }
 }
--- app/src/main/java/com/omkar/pheriwala/activities/customer/CustAccountActivity.java
+++ app/src/main/java/com/omkar/pheriwala/activities/customer/CustAccountActivity.java
@@ -1,18 +1,32 @@
 package com.omkar.pheriwala.activities.customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.omkar.pheriwala.R;
 
 public class CustAccountActivity extends AppCompatActivity {
 
-    Button btn_log, btn_reg;
+    Button btn_log, btn_reg, login_btn;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    TextView txxt_log_reg,title_txt;
+    EditText mail_log,pass_log;
+    String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,7 +70,45 @@ public class CustAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                //your login calculation goes here
+                login_btn = v.findViewById(R.id.login_btn);
+//                txxt_log_reg = v.findViewById(R.id.txxt_reg_logn);
+//                mail_log = v.findViewById(R.id.mail_login);
+//                pass_log = v.findViewById(R.id.pass_login);
+//                title_txt = view.findViewById(R.id.title_log_txt);
+
+                mAuth = FirebaseAuth.getInstance();
+
+
+                String mail = edtmail.getText().toString();
+                String pass = edtpass.getText().toString();
+
+
+                login_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        mAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(CustAccountActivity.this,"Login Success",Toast.LENGTH_LONG).show();
+//                            Intent i = new Intent(getContext(), MainActivity.class);
+//                            startActivity(i);
+
+                                    Intent i = new Intent(CustAccountActivity.this,CustHomeActivity.class);
+                                    startActivity(i);
+
+                                }
+                            }
+                        });
+
+
+                    }
+                });
+
+
+
+
             }
         });
 
